7145f18084703883062bbe4d232d669c85ab8b71
baechans <baechans@amazon.com>
2021-07-14 13:26:03 -0400

    Set up bastion host, refine cloud-init

--- scripts/get_hostnames.py
+++ scripts/get_hostnames.py
@@ -9,7 +9,6 @@ hostname = os.uname()[1]
 
 # error if argv length is not 2
 # sys.argv[1] = the bucket name to store the files
-# sys.argv[2] = the hostname
 
 hostnames = s3_client.list_objects_v2(Bucket=sys.argv[1], Prefix='hostnames/')['Contents']
 hostlist = []
@@ -24,9 +23,12 @@ hostlist.append(hostname)
 
 if len(hostlist) == 2:
     subprocess.call('pcs host auth ' + hostlist[0] + ' ' + hostlist[1], shell=True)
+    subprocess.call('pcs cluster setup ' + hostlist[0] + ' ' + hostlist[1], shell=True)
 elif len(hostlist) == 3:
     subprocess.call('pcs host auth ' + hostlist[0] + ' ' + hostlist[1] + ' ' + hostlist[2], shell=True)
+    subprocess.call('pcs cluster setup ' + hostlist[0] + ' ' + hostlist[1], shell=True)
 elif len(hostlist) == 4:
     subprocess.call('pcs host auth ' + hostlist[0] + ' ' + hostlist[1] + ' ' + hostlist[2] + ' ' + hostlist[3], shell=True)
+    subprocess.call('pcs cluster setup ' + hostlist[0] + ' ' + hostlist[1], shell=True)
 else:
     print('invalid number of hostnames')
