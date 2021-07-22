5f200433e1cf62ec6045876c547d40a25c70f9c4
baechans <baechans@amazon.com>
2021-07-22 16:33:21 -0400

    Commented out fencing for testing

--- scripts/get_hostnames.py
+++ scripts/get_hostnames.py
@@ -28,28 +28,28 @@ if len(hostlist) == 2:
     subprocess.call('sudo pcs host auth ' + hostlist[0] + ' ' + hostlist[1] + ' -u hacluster -p ' + sys.argv[2], shell=True)
     subprocess.call('sudo pcs cluster setup --name newcluster ' + hostlist[0] + ' ' + hostlist[1] + ' -u hacluster -p ' + sys.argv[2] + ' --force', shell=True)
     subprocess.call('sudo pcs cluster start --all --wait', shell=True)
-    hostmap = hostlist[0]+':'+instanceid[0]+';'+hostlist[1]+':'+instanceid[1]
-    subprocess.call('sudo pcs stonith create clusterfence fence_aws region=' + sys.argv[3] + ' pcmk_host_map=' + hostmap + ' power_timeout=240 pcmk_reboot_timeout=480 pcmk_reboot_retries=4 --force')
+    # hostmap = hostlist[0]+':'+instanceid[0]+';'+hostlist[1]+':'+instanceid[1]
+    # subprocess.call('sudo pcs stonith create clusterfence fence_aws region=' + sys.argv[3] + ' pcmk_host_map=' + hostmap + ' power_timeout=240 pcmk_reboot_timeout=480 pcmk_reboot_retries=4 --force')
 elif len(hostlist) == 3:
     subprocess.call('sudo pcs host auth ' + hostlist[0] + ' ' + hostlist[1] + ' ' + hostlist[2] + ' -u hacluster -p ' + sys.argv[2], shell=True)
     subprocess.call('sudo pcs cluster setup --name newcluster ' + hostlist[0] + ' ' + hostlist[1] + ' -u hacluster -p ' + sys.argv[2] + ' --force', shell=True)
     subprocess.call('sudo pcs cluster start --all --wait', shell=True)
-    hostmap = hostlist[0]+':'+instanceid[0]+';'+hostlist[1]+':'+instanceid[1]+';'+hostlist[2]+':'+instanceid[2]
-    subprocess.call('sudo pcs stonith create clusterfence fence_aws region=' + sys.argv[3] + ' pcmk_host_map=' + hostmap + ' power_timeout=240 pcmk_reboot_timeout=480 pcmk_reboot_retries=4 --force')
+    # hostmap = hostlist[0]+':'+instanceid[0]+';'+hostlist[1]+':'+instanceid[1]+';'+hostlist[2]+':'+instanceid[2]
+    # subprocess.call('sudo pcs stonith create clusterfence fence_aws region=' + sys.argv[3] + ' pcmk_host_map=' + hostmap + ' power_timeout=240 pcmk_reboot_timeout=480 pcmk_reboot_retries=4 --force')
 elif len(hostlist) == 4:
     subprocess.call('sudo pcs host auth ' + hostlist[0] + ' ' + hostlist[1] + ' ' + hostlist[2] + ' ' + hostlist[3] + ' -u hacluster -p ' + sys.argv[2], shell=True)
     subprocess.call('sudo pcs cluster setup --name newcluster ' + hostlist[0] + ' ' + hostlist[1] + ' -u hacluster -p ' + sys.argv[2] + ' --force', shell=True)
     subprocess.call('sudo pcs cluster start --all --wait', shell=True)
-    hostmap = hostlist[0]+':'+instanceid[0]+';'+hostlist[1]+':'+instanceid[1]+';'+hostlist[2]+':'+instanceid[2]+';'+hostlist[3]+':'+instanceid[3]
-    subprocess.call('sudo pcs stonith create clusterfence fence_aws region=' + sys.argv[3] + ' pcmk_host_map=' + hostmap + ' power_timeout=240 pcmk_reboot_timeout=480 pcmk_reboot_retries=4 --force')
+    # hostmap = hostlist[0]+':'+instanceid[0]+';'+hostlist[1]+':'+instanceid[1]+';'+hostlist[2]+':'+instanceid[2]+';'+hostlist[3]+':'+instanceid[3]
+    # subprocess.call('sudo pcs stonith create clusterfence fence_aws region=' + sys.argv[3] + ' pcmk_host_map=' + hostmap + ' power_timeout=240 pcmk_reboot_timeout=480 pcmk_reboot_retries=4 --force')
 else:
     print('invalid number of hostnames')
 
-subprocess.call('sudo pcs resource create ha_listener ocf:heartbeat:aws-vpc-move-ip ip=' + sys.argv[4] + ' interface="eth0" routing_table=' + sys.argv[5] + ' op monitor timeout="30s" interval="60s"', shell=True)
+# subprocess.call('sudo pcs resource create ha_listener ocf:heartbeat:aws-vpc-move-ip ip=' + sys.argv[4] + ' interface="eth0" routing_table=' + sys.argv[5] + ' op monitor timeout="30s" interval="60s"', shell=True)
 
-subprocess.call('sudo pcs property set stonith-enabled=true', shell=True)
-subprocess.call('sudo pcs property set start-failure-is-fatal=true', shell=True)
-subprocess.call('sudo pcs property set cluster-recheck-interval=75s', shell=True)
+# subprocess.call('sudo pcs property set stonith-enabled=true', shell=True)
+# subprocess.call('sudo pcs property set start-failure-is-fatal=true', shell=True)
+# subprocess.call('sudo pcs property set cluster-recheck-interval=75s', shell=True)
 
 
 
